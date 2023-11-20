712381bee9a63f1bcd4eec9121423de9f476cbe6
AnyaMarcanito <19-10336@usb.ve>
2023-11-20 00:42:11 -0400

    Modificaciones de NextToYou.java Estado: Funcional

--- Laboratorio #2/NextToYou.java
+++ Laboratorio #2/NextToYou.java
@@ -3,243 +3,176 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
-/*class NextToYou {
-    private static int numComponente;
+public class NextToYou {
 
-    public static Map<String, Integer> componentesFuertementeConexas(Graph<String> graph) {
-        Map<String, Integer> componentes = new HashMap<>();
-        List<String> f = new ArrayList<>();
-        Map<String, Boolean> visitados = new HashMap<>();
-        dfsVisit(graph, visitados, componentes, f);
-        
-        Graph<String> simetricGraph = graph.getSimetric();
+    /**
+     * Calcula las componentes fuertemente conexas de un grafo dado.
+     * Utiliza el algoritmo de búsqueda en profundidad (DFS) para encontrar las componentes.
+     * 
+     * @param graph el grafo en el que se buscarán las componentes fuertemente conexas
+     * @return una lista de listas que contiene las componentes fuertemente conexas del grafo
+     */
+    public static List<List<String>> calculoDeComponentesFuertementeConexas(Graph<String> graph) {
+        //Inicializamos un conjunto de visitados, una lista de finalizados y una lista para las componentes 
+        //fuertemente conexas
+        Set<String> visitados = new HashSet<>();
+        List<String> finalizados = new ArrayList<>();
+        List<List<String>> components = new ArrayList<>();
 
-        Collections.reverse(f);
+        //Recorremos todos los vertices del grafo.
+        for (String vertex : graph.getAllVertices()) {
+            //Si el vertice no ha sido visitado entonces se llama al metodo dfs().
+            if (!visitados.contains(vertex)) {
+                dfs(graph, vertex, visitados, finalizados, true);
+            }
+        }
 
-        List<String> vertices = f;
-
+        //Vaciamos el conjunto de visitados.
         visitados.clear();
-        f.clear();
-        componentes.clear();
+                
 
-        dfsVisit2(simetricGraph, visitados, componentes, f, vertices);
+        //Recorremos la lista de finalizados en orden inverso.
+        Collections.reverse(finalizados);
 
-        return componentes;
-    }
+        //Creamos un grafo simetrico al grafo original.
+        Graph<String> simetrico = graph.getSimetric();
 
-    private static void dfsVisit(Graph<String> graph, Map<String, Boolean> visitados, Map<String, Integer> componentes, List<String> f) {
-        numComponente = 0;
-
-        for (String vertex : graph.getAllVertices()) {
-            visitados.put(vertex, false);
-            f.add(vertex);
-        }
-
-        for (String vertex : graph.getAllVertices()) {
-            if (!visitados.getOrDefault(vertex, false)) {
-                dfs(graph, visitados, componentes, f, vertex);
-                numComponente++;
+        //Recorremos todos los vertices del grafo.
+        for (String vertex : finalizados) {
+            //Si el vertice no ha sido visitado entonces se llama al metodo dfsTranspose() 
+            //y se inicializa una lista para la componenete fuertemente conexa del vertice actual.
+            if (!visitados.contains(vertex)) {
+                List<String> component = new ArrayList<>();
+                dfs(simetrico, vertex, visitados, component, false);
+                components.add(component);
             }
         }
-    }
-
-    private static void dfsVisit2(Graph<String> graph, Map<String, Boolean> visitados, Map<String, Integer> componentes, List<String> f, List<String> vertices) {
-        numComponente = 0;
-
-        for (String vertex : graph.getAllVertices()) {
-            visitados.put(vertex, false);
-            f.add(vertex);
-        }
-
-        List<String> verticesCopy = new ArrayList<>(vertices);
-        for (String vertex : verticesCopy) {
-            if (!visitados.getOrDefault(vertex, false)) {
-                dfs(graph, visitados, componentes, f, vertex);
-                numComponente++;
-            }
-        }
-    }
-
-    private static void dfs(Graph<String> graph, Map<String, Boolean> visitados, Map<String, Integer> componentes, List<String> f, String vertex) {
-        visitados.put(vertex, true);
-        componentes.put(vertex, numComponente);
-
-        for (String vecino : graph.getOutwardEdges(vertex)) {
-            if (!visitados.getOrDefault(vecino, false)) {
-                dfs(graph, visitados, componentes, f, vecino);
-            }
-        }
-        f.add(vertex);
-    }
-
-    private static int maximoValorDeMap(Map<String, Integer> localidades){
-        int maxValue = Integer.MIN_VALUE;
-        for (Integer value : localidades.values()) {
-            if (value.compareTo(maxValue) > 0) {
-                maxValue = value;
-            }
-        }
-        return maxValue;
-    }
-
-    private static List<List<String>> listasDeLocalidades(Map<String, Integer> localidades) {
-        int maxValue = maximoValorDeMap(localidades);
-        
-        List<List<String>> listas = new ArrayList<>();
-
-        for (int i = 0; i <= maxValue; i++) {
-            List<String> list = new ArrayList<>();
-            for (Map.Entry<String, Integer> entry : localidades.entrySet()) {
-                String key = entry.getKey();
-                Integer value = entry.getValue();
-                if (value == i) {
-                    list.add(key);
-                }
-            }
-            listas.add(list);
-        }
-        return listas;
-    }
-
-    private static int numeroDeRepartidores(Map<String, Integer> localidades) {
-        int numRepartidores = 0;
-        
-        List<List<String>> listasDeLocalidades = listasDeLocalidades(localidades);
-
-        for (List<String> localidad : listasDeLocalidades) {
-            int numComercios = localidad.size();
-            if (numComercios <= 2) {
-                numRepartidores += 10;
-                System.out.println("2");
-            } else if (numComercios <= 5) {
-                numRepartidores += 20;
-                System.out.println("5");
-            } else {
-                numRepartidores += 30;
-                System.out.println("mas");
-            }
-        }
-        return numRepartidores;
+        //Retornamos la lista de componentes.
+        return components;
     }
     
-    public static void main(String[] args) {
-        // Creamos un grafo vacío.
-        Graph<String> graph = new AdjacencyListGraph<>();
-        try {
-            File inputFile = new File("Caracas.txt");
-            Scanner scanner = new Scanner(inputFile);
-
-            //Iteramos sobre cada linea del archivo.
-            while (scanner.hasNextLine()) {
-                // Leemos la línea y la separamos por comas.
-                String line = scanner.nextLine();
-                String[] data = line.split(",");
-                // Identificamos los dos comercios de cada linea y los agregamos como vertices al grafo.
-                String comercio1 = data[0];
-                String comercio2 = data[1];
-                graph.add(comercio1);
-                graph.add(comercio2);
-                // Como existe una via de poco trafico entre comercio1 y comercio2 agregamos
-                // un arco entre ellos.
-                graph.connect(comercio1, comercio2);
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            //Si no se encuentra el archivo input.txt se imprime un mensaje de error.
-            System.out.println("No se pudo encontrar el archivo Caracas.txt.");
-            return;
+    /**
+     * Imprime las localidades encontradas.
+     * 
+     * @param localidades la lista de localidades a imprimir
+     */
+    private static void imprimirLocalidades(List<List<String>> localidades) {
+        System.out.println("Localidades encontradas:");
+        for (int i = 0; i < localidades.size(); i++) {
+            System.out.println("Localidad " + (i + 1) + ": " + localidades.get(i));
         }
-        // Calculamos las componentes fuertemente conexas del grafo, ellas seran las localidades para el problema.
-        Map<String, Integer> localidades = componentesFuertementeConexas(graph);
-        // Calculamos el numero de repartidores necesarios para cubrir todas las localidades.
-        int numRepartidores = numeroDeRepartidores(localidades);
-        // Imprimimos el resultado por la salida estandar.
-        System.out.println("Cantidad de repartidores necesarios: " + numRepartidores);
     }
-}
-*/
-public class NextToYou {
-    public static void main(String[] args) {
-        Map<String, Set<String>> adjacencyMap = new HashMap<>();
 
+    /**
+     * Calcula el número total de conductores necesarios para cubrir todas las localidades.
+     * 
+     * @param localidades una lista de listas de cadenas que representa las localidades y sus comercios
+     * @return el número total de conductores necesarios
+     */
+    private static int calcularTotalDeConductores(List<List<String>> localidades) {
+        int totalConductores = 0;
+        //Iteramos sobre las localidades.
+        for (List<String> locality : localidades) {
+            //Vemos la cantidad de comercios que tiene cada localidad y acumulamos el numero 
+            //de conductores en totalConductores.
+            int size = locality.size();
+            if (size <= 2) {
+                totalConductores += 10;
+            } else if (size <= 5) {
+                totalConductores += 20;
+            } else {
+                totalConductores += 30;
+            }
+        }
+        //Retornamos el numero total de conductores.
+        return totalConductores;
+    }
+
+    /**
+     * Realiza una búsqueda en profundidad (DFS) en el grafo dado.
+     * Si cualDFS es true, entonces en la busqueda DFS se busca generar la lista de finalizados.
+     * Si cualDFS es false, entonces en la busqueda DFS se busca generar las componentes fuertemente conexas.
+     * 
+     * @param graph el grafo en el que se realizará la búsqueda
+     * @param vertex el vértice actual
+     * @param visitados un conjunto de vértices visitados
+     * @param finalizadosOComponentes una lista de vértices finalizados o lista de vertices de una componente fuertemente 
+     * conexa
+     * @param cualDFS un booleano que indica si se trata del primer o segundo DFS
+     */
+    private static void dfs(Graph<String> graph, String vertex, Set<String> visitados, List<String> finalizadosOComponentes, boolean cualDFS) {
+        //Si cualDFS es true entonces se busca generar la lista de finalizados.
+        if (cualDFS == true){
+            //Agregamos el vertice vertex al conjunto de visitados.
+            visitados.add(vertex);
+            //Recorremos todos los sucesores del vertice vertex.
+            for (String vecino : graph.getOutwardEdges(vertex)) {
+                //Si el sucesor no ha sido visitado entonces se llama al metodo dfs().
+                if (!visitados.contains(vecino)) {
+                    dfs(graph, vecino, visitados, finalizadosOComponentes, true);
+                }
+            }
+            //Cuando ya hemos recorrido todos los sucesores de un vertice lo agregamos a la lista de finalizados.
+            finalizadosOComponentes.add(vertex);
+        //Si cualDFS es false entonces se busca generar las componentes fuertemente conexas.
+        } else {
+            //Agregamos el vertice vertex al conjunto de visitados y a la lista de componentes fuertemente conexas.
+            visitados.add(vertex);
+            finalizadosOComponentes.add(vertex);
+            //Recorremos todos los predecesores del vertice vertex
+            for (String vecino : graph.getOutwardEdges(vertex)) {
+                //Si el sucesor no ha sido visitado entonces se llama al metodo dfs().
+                if (!visitados.contains(vecino)) {
+                    dfs(graph, vecino, visitados, finalizadosOComponentes, false);
+                }
+            }
+        }
+    }
+
+    /**
+     * El método main es el punto de entrada del programa.
+     * -> Lee un archivo Caracas.txt que contiene pares de nombres de comercios.
+     * -> Crea un grafo donde los vertices son los nombres de los comercios y los 
+     *    arcos representan las vias con poco trafico que los conectan.
+     * -> Por lo tanto el grafo resultante representa una red de localidades y sus
+     *    conexiones.
+     * -> Calcula las localidades para el problema como las componentes fuertemente
+     *    conexas y en base a ellas calcula el número de conductores necesarios para
+     *    cubrir la red de localidades.
+     */
+    public static void main(String[] args) {
+        //Creamos un grafo de tipo AdjacencyListGraph.
+        Graph<String> graph = new AdjacencyListGraph<>();
+        //Leemos el archivo Caracas.txt y agregamos los nombres al grafo.
         try (BufferedReader br = new BufferedReader(new FileReader("Caracas.txt"))) {
             String line;
+            //Leemos el archivo linea por linea.
             while ((line = br.readLine()) != null) {
+                //Separamos los nombres de las localidades por la coma del archivo Caracas.txt.
                 String[] commercePair = line.split(",");
                 String commerce1 = commercePair[0].trim();
                 String commerce2 = commercePair[1].trim();
-
-                adjacencyMap.computeIfAbsent(commerce1, k -> new HashSet<>()).add(commerce2);
+                //Agregamos los nombres de las localidades al grafo.
+                graph.add(commerce1);
+                graph.add(commerce2);
+                //Conectamos los comercios exactamente en la direccion en la que aparecen en el archivo Caracas.txt.
+                graph.connect(commerce1, commerce2);
             }
+            //Si no se encuentra el archivo Caracas.txt se imprime un mensaje de error.
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
-
-        List<Set<String>> localities = findLocalities(adjacencyMap);
-        printLocalities(localities);
-
-        int totalDrivers = calculateTotalDrivers(localities);
-        System.out.println("Número total de repartidores necesarios: " + totalDrivers);
-    }
-
-    private static List<Set<String>> findLocalities(Map<String, Set<String>> adjacencyMap) {
-        List<Set<String>> localities = new ArrayList<>();
-        Set<String> visited = new HashSet<>();
-
-        for (String commerce : adjacencyMap.keySet()) {
-            if (!visited.contains(commerce)) {
-                Set<String> locality = new HashSet<>();
-                dfs(commerce, adjacencyMap, visited, locality);
-                localities.add(locality);
-            }
-        }
-
-        return localities;
-    }
-
-    private static void dfs(String commerce, Map<String, Set<String>> adjacencyMap, Set<String> visited, Set<String> locality) {
-        visited.add(commerce);
-        locality.add(commerce);
-
-        Set<String> neighbors = adjacencyMap.get(commerce);
-        if (neighbors != null) {
-            for (String neighbor : neighbors) {
-                if (!visited.contains(neighbor)) {
-                    dfs(neighbor, adjacencyMap, visited, locality);
-                }
-            }
-        }
-    }
-
-    private static void printLocalities(List<Set<String>> localities) {
-        System.out.println("Localidades encontradas:");
-        for (int i = 0; i < localities.size(); i++) {
-            System.out.println("Localidad " + (i + 1) + ": " + localities.get(i));
-        }
-    }
-
-    private static int calculateTotalDrivers(List<Set<String>> localities) {
-        int totalDrivers = 0;
-        for (Set<String> locality : localities) {
-            int size = locality.size();
-            if (size <= 2) {
-                totalDrivers += 10;
-            } else if (size <= 5) {
-                totalDrivers += 20;
-            } else {
-                totalDrivers += 30;
-            }
-        }
-        return totalDrivers;
+        //Conseguimos las localidades del problema usando el metodo findcalculoDeComponentesFuertementeConexas(), 
+        //pues la definicion de localidad para este problema coincide con la de componentes fuertemente conexas.
+        List<List<String>> localidades = calculoDeComponentesFuertementeConexas(graph);
+        //Imprimimos las localidades encontradas.
+        imprimirLocalidades(localidades);
+        //Calculamos el numero total de repartidores necesarios.
+        int totalConductores = calcularTotalDeConductores(localidades);
+        //Imprimimos el numero total de repartidores necesarios.
+        System.out.println("Número total de repartidores necesarios: " + totalConductores);
     }
 }
